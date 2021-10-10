6dde5741e9f732b3e8280aa39c2f829f98330afe
Anubhav Paras <anubhavparas94@gmail.com>
2021-10-10 00:00:56 -0400

    fixed second bug - memory leak - used smart pointer. can be fixed by deleting the refernce to the earlier raw pointer too

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,22 +1,22 @@
+/** Copyright 2021 Anubhav Paras */
+
 #include <AnalogSensor.hpp>
 #include <numeric>
 #include <vector>
+#include <memory>
 
 AnalogSensor::AnalogSensor(unsigned int samples)
     : mSamples(samples)
-{
-}
+{}
 
 AnalogSensor::~AnalogSensor()
-{
-}
+{}
 
-int AnalogSensor::Read()
-{
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
-
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+int AnalogSensor::Read() {
+    std::unique_ptr<std::vector<int>> readings(
+        new std::vector<int>(mSamples, 10));
+    double result = std::accumulate(
+                        readings->begin(), readings->end(), 0.0) /
+                    readings->size();
     return result;
 }
-
-
